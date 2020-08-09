c0657c5a5c1a829e33ce552aafd34eddb3460f38
CameronJYoung <cameronjyoung123@gmail.com>
2020-08-09 16:27:04 +0100

    added browserify that bundles javascript for each page dynamically

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
+//Libraries
+
 const gulp = require('gulp');
-const twig = require('gulp-twig');
+const del = require("del")
+const fs = require("fs")
+
+//CSS
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const scss = require('gulp-sass');
-const babel = require('gulp-babel');
-
 scss.compiler = require('node-sass');
+//Templates
+const twig = require('gulp-twig');
+//Javascript
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename")
+const es = require("event-stream")
+
 
 
 let templatesTask = () => {
@@ -34,16 +48,52 @@ let cssTask = () => {
 
 }
 
-let jsConvert = () => {
-	return gulp.src('./app/scripts/**/*.js')
-		.pipe(babel({
-			presets: ['@babel/env']
-		}))
-		.pipe(gulp.dest('./dist/scripts'))
+let jsConvert = (done) => {
+	var fileArray = [
+		'./app/scripts/main.js',
+	];
+	
+	var pagesFilePrefix = './app/scripts/pages/';
+
+
+	fs.readdir('./app/scripts/pages/', (err, files) => {
+		files.forEach(file => {
+			fileArray.push(pagesFilePrefix + file);
+			
+		})
+		var tasks = fileArray.map(function(entry) {
+			return browserify({
+				entries: [entry],
+				transform: [babelify.configure({presets:['@babel/preset-env']})]
+			})
+			.bundle()
+			.pipe(source(entry.replace('./app/scripts/','').replace('.js','').replace('pages/','')))
+			.pipe(rename({
+				extname: '.bundle.js'
+			}))
+			.pipe(buffer())
+			.pipe(uglify())
+			.pipe(gulp.dest('./dist/scripts'))
+		});
+		es.merge.apply(null, tasks);
+	});
+
+    // map them to our stream function
+    
+	done();
+};
+
+let cleanTMP = () => {
+	return del('./.tmp')
+}
+
+let cleanDIST = () => {
+	return del('./dist')
 }
 
 
 exports.templates = templatesTask
 exports.css = cssTask
 exports.scss = scssTask
-exports.build = gulp.series(templatesTask,scssTask,cssTask,jsConvert)
+exports.clean = cleanTMP
+exports.build = gulp.series(cleanDIST,templatesTask,scssTask,cssTask,jsConvert,cleanTMP)
