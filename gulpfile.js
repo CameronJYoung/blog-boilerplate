10a50be3683d91ef281d11beaefc8c53c60f33fc
CameronJYoung <cameronjyoung123@gmail.com>
2020-08-09 20:47:43 +0100

    made all paths part of an object at the top of the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -18,26 +18,42 @@ const rename = require("gulp-rename")
 const es = require("event-stream")
 
 const paths = {
-	fonts: '',
-	imgs: '',
-	styles: '',
-	templates: ''
+	in : {
+		app: './app',
+		fonts: './app/fonts',
+		imgs: './app/imgs',
+		scripts: './app/scripts',
+		styles: './app/styles/',
+		templates: {
+			include: './app/templates/includes',
+			layout: './app/templates/layouts',
+			page: './app/templates/pages'
+		}
+	},
+	out : {
+		tmp: './.tmp',
+		dist: './dist',
+		fonts: './dist/fonts',
+		imgs: './dist/imgs',
+		scripts: './dist/scripts',
+		styles: './dist/styles'
+	}
 }
 
 
 
 const templatesTask = () => {
-	return gulp.src('./app/templates/pages/*.html')
+	return gulp.src(`${paths.in.templates.page}/*.html`)
 		.pipe(twig())
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest(`${paths.out.dist}/`))
 		.pipe(browserSync.stream());
 
 }
 
 const scssTask = () => {
-	return gulp.src('./app/styles/**/*.scss')
+	return gulp.src(`${paths.in.styles}/**/*.scss`)
 		.pipe(scss().on('error', scss.logError))
-		.pipe(gulp.dest('./.tmp/styles/'))
+		.pipe(gulp.dest(`${paths.out.tmp}/styles/`))
 
 }
 
@@ -46,23 +62,22 @@ const cssTask = () => {
 		autoprefixer({overrideBrowserslist: ['last 2 versions']}),
 		cssnano()
 	]
-	return gulp.src('./.tmp/styles/**/*.css')
+	return gulp.src(`${paths.out.tmp}/styles/**/*.css`)
 		.pipe(postcss(processors))
-		.pipe(gulp.dest('./dist/styles'))
+		.pipe(gulp.dest(paths.out.styles))
 		.pipe(browserSync.stream())
 
 }
 
 const jsConvert = (done) => {
 	var fileArray = [
-		'./app/scripts/main.js',
+		`${paths.in.scripts}/main.js`
 	];
-	var pagesFilePrefix = './app/scripts/pages/';
+	var pagesFilePrefix = `${paths.in.scripts}/pages/`;
 	fs.readdir(pagesFilePrefix, (err, files) => {
-		if (err) done(err);
+		//if (err) done(err);
 		files.forEach(file => {
 			fileArray.push(pagesFilePrefix + file);
-			
 		})
 		var tasks = fileArray.map((entry) => {
 			return browserify({
@@ -70,13 +85,13 @@ const jsConvert = (done) => {
 				transform: [babelify.configure({presets:['@babel/preset-env']})]
 			})
 			.bundle()
-			.pipe(source(entry.replace('./app/scripts/','').replace('.js','').replace('pages/','')))
+			.pipe(source(entry.replace(`${paths.in.scripts}/`,'').replace('.js','').replace('pages/','')))
 			.pipe(rename({
 				extname: '.bundle.js'
 			}))
 			.pipe(buffer())
 			.pipe(uglify())
-			.pipe(gulp.dest('./dist/scripts'))
+			.pipe(gulp.dest(paths.out.scripts))
 			.pipe(browserSync.stream())
 		});
 		es.merge.apply(null, tasks);
@@ -86,43 +101,43 @@ const jsConvert = (done) => {
 };
 
 const moveFontsTask = () => {
-	return gulp.src('./app/fonts/**/*.*')
-		.pipe(gulp.dest('./dist/fonts'));
+	return gulp.src(`${paths.in.fonts}/**/*.*`)
+		.pipe(gulp.dest(paths.out.fonts));
 }
 
 const delOldFontsTask = () => {
-	return del('./dist/fonts')
+	return del(paths.out.fonts)
 }
 
 const moveImgsTask = () => {
-	return gulp.src('./app/imgs/**/*.*')
-		.pipe(gulp.dest('./dist/imgs'))
+	return gulp.src(`${paths.in.imgs}/**/*.*`)
+		.pipe(gulp.dest(paths.out.imgs))
 }
 
 const delOldImgsTask = () => {
-	return del('./dist/imgs')
+	return del(paths.out.imgs)
 }
 
 const cleanTMP = () => {
-	return del('./.tmp')
+	return del(paths.out.tmp)
 }
 
 const cleanDIST = () => {
-	return del('./dist')
+	return del(paths.out.dist)
 }
 
 const serveTask = () => {
 	browserSync.init({
         server: {
-			baseDir: "./dist/"
+			baseDir: `${paths.out.dist}/`
         }
     });
 
-	gulp.watch('./app/styles/**/*').on('change', gulp.series(scssTask,cssTask,cleanTMP));
-	gulp.watch('./app/scripts/**/*').on('change',jsConvert);
-	gulp.watch('./app/templates/**/*').on('change', templatesTask);
-	gulp.watch('./app/fonts/**/*').on('change', gulp.series(delOldFontsTask,moveFontsTask,reload));
-	gulp.watch('./app/imgs/**/*').on('change', gulp.series(delOldImgsTask,moveImgsTask,reload));
+	gulp.watch(`${paths.in.styles}/**/*`).on('change', gulp.series(scssTask,cssTask,cleanTMP));
+	gulp.watch(`${paths.in.scripts}/**/*`).on('change',jsConvert);
+	gulp.watch(`${paths.in.app}/templates/**/*`).on('change', templatesTask);
+	gulp.watch(`${paths.in.fonts}/**/*`).on('change', gulp.series(delOldFontsTask,moveFontsTask,reload));
+	gulp.watch(`${paths.in.imgs}/**/*`).on('change', gulp.series(delOldImgsTask,moveImgsTask,reload));
 }
 
 
@@ -132,5 +147,6 @@ exports.default = gulp.series(cleanDIST,gulp.parallel(templatesTask,gulp.series(
 
 exports.dev = gulp.series(cleanDIST,gulp.parallel(templatesTask,gulp.series(scssTask,cssTask),jsConvert,moveFontsTask,moveImgsTask),cleanTMP,serveTask)
 exports.clean = cleanTMP
+exports.js = jsConvert
 
 exports.build = gulp.series(cleanDIST,gulp.parallel(templatesTask,gulp.series(scssTask,cssTask),jsConvert,moveFontsTask,moveImgsTask),cleanTMP)
